04041077d22fe262400f0bdbf9d82efd294fca39
yuzutyaso <f.kokori85@gmail.com>
2025-05-31 19:14:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def getRandomUserAgent():
 
 class InvidiousAPI:
     def __init__(self):
-        self.all = ast.literal_eval(requests.get('https://raw.githubusercontent.com/lunakamituki/yukiyoutube-inv-instances/refs/heads/main/main.txt', headers=getRandomUserAgent(), timeout=(1.0, 0.5)).text)
+        self.all = ast.literal_eval(requests.get('https://raw.githubusercontent.com/yuzutyaso/yyu/refs/heads/main/main.txt', headers=getRandomUserAgent(), timeout=(1.0, 0.5)).text)
         
         self.video = self.all['video']
         self.playlist = self.all['playlist']
