13bf5613707dce76b74e2d53cca282e8e41ecdf3
yuzutyaso <f.kokori85@gmail.com>
2025-06-02 22:55:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ import ast
 
 
 # 3 => (3.0, 1.5) => (1.5, 1)
-max_api_wait_time = (1.5, 1)
+max_api_wait_time = (20, 1)
 # 10 => 10
-max_time = 10
+max_time = 100
 
 user_agents = [
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15',
